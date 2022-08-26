a11375a4d6272d0ded548e24bfdc2c7adfdc9f19
Joongi Kim <me@daybreaker.info>
2022-08-26 18:00:09 +0900

    fix: Simplify col name

--- aiomonitor/monitor.py
+++ aiomonitor/monitor.py
@@ -379,7 +379,7 @@ class Monitor:
     @alt_names('p')
     def do_ps(self) -> None:
         """Show task table"""
-        headers = ('Task ID', 'State', 'Name', 'Coroutine', 'Created Location', 'Since Created')
+        headers = ('Task ID', 'State', 'Name', 'Coroutine', 'Created Location', 'Since')
         table_data: List[Tuple[str, str, str, str, str, str]] = [headers]
         for task in sorted(all_tasks(loop=self._loop), key=id):
             taskid = str(id(task))
